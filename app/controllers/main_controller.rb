b92f09089617d3b07dcaf7dbecdd1c0c324f24ff
Nathaniel Truitt <nathantruitt2@gmail.com>
2022-12-15 07:51:36 -0600

    some controllas and views

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,6 @@
 
 class MainController < ApplicationController
   def index
-    flash.now[:notice] = "Logged in successfully"
-    flash.now[:alert] = "Invalid email or password"
+
   end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.find_by email: params[:email]
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully!"
+    else
+      flash[:alert] = "Invalid username or password"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "You have logged out successfully!"
+  end
+end
